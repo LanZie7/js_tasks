1efa8ed8dc85b159f3ef3ef4efb537e6a4f570eb
bochkova_su <bochkova_su@resanta.ru>
2024-03-13 18:20:28 +0300

    2 solutions for createBadge function

--- fullNameBadge.js
+++ fullNameBadge.js
@@ -1,47 +1,91 @@
 /**
 
-Задача 1
+ЗАДАЧА 1
 
 Наши сотрудники хотят бейджики, поэтому нужно написать функцию, которая поможет нам их создавать.
 
 На вход она ждет имя и фамилию, а возвращает объект вида: 
 { firstName, lastName, fullName }, где каждое свойство можно читать и менять.
 
-Если предметом изменений является firstName или lastName, то правим fullName. Если же это fullName, то заменяем firstName и lastName. На примере с кодом станет понятнее.
+Если предметом изменений является firstName или lastName, то правим fullName. Если же это fullName, то заменяем firstName и lastName.
 */
 
-const createBadge = (firstName, lastName) => {
-    let fullName = `${firstName} ${lastName}`;
+// Решение 1
+// const createBadge = (firstName, lastName) => {
+//     let _firstName = firstName;
+//     let _lastName = lastName;
 
+//     return {
+//         get firstName() {
+//             return _firstName;
+//         },
+//         set firstName(newFirstName) {
+//             _firstName = newFirstName;
+//             this.fullName = `${_firstName} ${_lastName}`;
+//         },
+//         get lastName() {
+//             return _lastName;
+//         },
+//         set lastName(newLastName) {
+//             _lastName = newLastName;
+//             this.fullName = `${_firstName} ${_lastName}`;
+//         },
+//         get fullName() {
+//             return `${_firstName} ${_lastName}`;
+//         },
+//         set fullName(newFullName) {
+//             const [newFirstName, newLastName] = newFullName.split(' ');
+//             if(newFirstName && newLastName) {
+//                 _firstName = newFirstName;
+//                 _lastName = newLastName;
+//             } else {
+//                 console.log('Invalid fullName format');
+//             }
+//         }
+//     };
+// };
+
+
+// Решение 2
+const createBadge = (firstName, lastName) => {
     return {
-        get firstName() {
-            return firstName;
-        },
-        set firstName(newFirstName) {
-            firstName = newFirstName;
-            fullName = `${firstName} ${lastName}`;
-        },
-        get lastName() {
-            return lastName;
-        },
-        set lastName(newLastName) {
-            lastName = newLastName;
-            fullName = `${firstName} ${lastName}`;
-        },
+        firstName,
+        lastName,
         get fullName() {
-            return fullName;
+            return `${this.firstName} ${this.lastName}`;
         },
-        set fullName(newFullName) {
-            const nameParts = newFullName.split(' ');
-            firstName = nameParts[0];
-            lastName = nameParts[1];
-            fullName = newFullName;
-        }
+        set fullName(val) {
+            const dividedName = val.split(' ');
+            if (dividedName.length !== 2) {
+            console.log('Error: invalid fullName format');
+            }
+            [this.firstName, this.lastName] = dividedName;
+        },
     };
 };
 
+const badge = createBadge("Иван", "Иванов");
 
-const badge = createBadge('Lana', 'Bochkova');
-console.log(badge.firstName); // Lana
-console.log(badge.lastName); // Bochkova
-console.log(badge.fullName); // Lana Bochkova
+console.log(badge.firstName); // Иван
+console.log(badge.lastName); // Иванов
+console.log(badge.fullName); // Иван Иванов
+
+badge.firstName = 'Константин';
+
+console.log(badge.firstName); // Константин
+console.log(badge.lastName); // Иванов
+console.log(badge.fullName); // Константин Иванов
+
+badge.lastName = 'Петров';
+
+console.log(badge.firstName); // Константин
+console.log(badge.lastName); // Петров
+console.log(badge.fullName); // Константин Петров
+
+badge.fullName = 'Илон Маск';
+
+console.log(badge.firstName); // Илон
+console.log(badge.lastName); // Маск
+console.log(badge.fullName); // Илон Маск
+
+badge.fullName = 'ИлонМаск'; // Error: invalid fullName format
